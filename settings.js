b17798f04545b9bf2546e63bf68ff7235bac1d57
zdndd <dongningdella@gmail.com>
2014-01-08 18:14:26 +0800

    se

--- settings.js
+++ settings.js
@@ -3,10 +3,10 @@
  /*数据库连接信息host,port,user,pwd*/
 
 module.exports = {   
-	cookieSecret: 'myblog',   
-	db_name : 'fWGaGFyOOWRGrwcXkKaD',                 // 数据库名，从云平台获取
+	cookieSecret: '***',   
+	db_name : '***',                 // 数据库名，从云平台获取
 	db_host :  'mongo.duapp.com',     // 数据库地址
 	db_port :  '8908',   // 数据库端口
-	username : 'lyyflUq8eBBPyDhlbz803f4V',                 // 用户名（API KEY）
-	password : 'P68ko70GvcCW0r6DDkgFfojj8KgPRjHA'                 // 密码(Secret KEY)
+	username : '***',                 // 用户名（API KEY）
+	password : '***'                 // 密码(Secret KEY)
 };   
